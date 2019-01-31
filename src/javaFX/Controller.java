72c8bffe4c57dfb7dac77b5d4d3107753ad04c7a
Sebastian <folusz.sebastian@gmail.com>
2019-01-31 12:39:59 +0100

    Zrobienie funkcjonalnosci zwiekszenia ceny produktu o dany procent

--- src/javaFX/Controller.java
+++ src/javaFX/Controller.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import services.ProductPrice;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -54,21 +55,56 @@ public class Controller implements Initializable {
         columnAmount.setCellValueFactory(new PropertyValueFactory<>("amount"));
 
         dataList = FXCollections.observableArrayList();
-        table.setItems(dataList);
         products = new ArrayList<>();
+        table.setItems(dataList);
 
         bLoadData.setOnAction(event -> {
-            dataList.clear();
-            products.clear();
             loadData();
             tab1.setDisable(false);
             tab2.setDisable(false);
             tab3.setDisable(false);
             tab4.setDisable(false);
         });
+
+
+        tab1.setDisable(false);
+        bChangePrices.setOnAction(event -> {
+            String number = tfProcentValue.getText();
+
+            if (isNumeric(number)){
+                String sign = tfProcentValue.getText().substring(0,1);
+                double value = Double.parseDouble(tfProcentValue.getText().substring(1));
+
+                if (sign.equals("+")) {
+                    System.out.println("Podwyzka o: " + value + "%");
+                    System.out.println();
+                    new ProductPrice().increaseDecrease(
+                            products.toArray(new Product[products.size()]), value, 0
+                    );
+                } else {        // sign: -
+                    System.out.println("Obnizka o: " + value + "%");
+                    System.out.println();
+                    new ProductPrice().increaseDecrease(
+                            products.toArray(new Product[products.size()]), value, 1
+                    );
+                }
+            } else {
+                System.out.println("Niepoprawny format");
+            }
+        });
+    }
+
+    private void updateDataList(double value, ProductPrice productPrice, int sign) {
+//        dataList.clear();
+//        Product[] newProducts = productPrice.increaseDecrease(
+//                products.toArray(new Product[products.size()]), value, sign
+//        );
+//        dataList.add(newProducts);
     }
 
     private void loadData() {
+        dataList.clear();
+        products.clear();
         String CsvFile = "dane_aplikacji.csv";
         String FieldDelimiter = ";";
 
@@ -82,9 +118,10 @@ public class Controller implements Initializable {
                 String[] fields = line.split(FieldDelimiter);
 
                 Product product = new Product(Integer.parseInt(fields[0]), fields[1], Double.parseDouble(fields[2]), Integer.parseInt(fields[3]));
-                products.add(product);
                 dataList.add(product);
+                products.add(product);
             }
+
 //            System.out.println("prod. id: " + products.get(0).getId());
 
         } catch (FileNotFoundException e) {
@@ -92,5 +129,9 @@ public class Controller implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static boolean isNumeric(String str) {
+        return str.matches("((-?)|(\\+?))\\d+(\\.\\d+)?");
     }
 }
---
+++ src/services/ProductPrice.java
@@ -0,0 +1,49 @@
+package services;
+
+import data.Product;
+import org.jruby.embed.ScriptingContainer;
+
+public class ProductPrice {
+
+    public void increaseDecrease(Product[] products, Double percent, Integer sign) {
+        ScriptingContainer container = new ScriptingContainer();
+        String script =
+                "class Product\n" +
+                    "attr_accessor :id, :name, :price, :amount\n" +
+                "end\n" +
+                "def method(mArray, percent, sign)\n" +
+                        "    \n" +
+                        "    if sign == 0\n" +
+                        "        percent = 1 + percent / 100\n" +
+                        "    else\n" +
+                        "        percent = 1 - percent / 100\n" +
+                        "    end\n" +
+                        "    \n" +
+                        "    newProducts = []\n" +
+                        "    for array in mArray\n" +
+                        "        newProduct = Product.new()\n" +
+                        "    \n" +
+                        "        newProduct.price = (array.price*percent).round(2)\n" +
+                        "        newProduct.name = array.name\n" +
+                        "        newProduct.id = array.id\n" +
+                        "        newProduct.amount = array.amount\n" +
+                        "        newProducts.push(newProduct)\n" +
+                        "    end\n" +
+                        "\n" +
+                        "    for product in newProducts\n" +
+                        "        puts \"new prod.: \"\n" +
+                        "        puts product.id\n" +
+                        "        puts product.price\n" +
+                        "        puts product.name\n" +
+                        "        puts product.amount\n" +
+                        "        puts \"\"\n" +
+                        "    end\n" +
+                "end";
+        Object receiver = container.runScriptlet(script);
+        Object[] args = new Object[3];
+        args[0] = products;
+        args[1] = percent;
+        args[2] = sign;
+        container.callMethod(receiver, "method", args, null);
+    }
+}
